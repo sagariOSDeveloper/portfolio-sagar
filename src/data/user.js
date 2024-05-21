c9a4f465ef27ba57ae0b2ab8a30839efd2f9ee20
Sagar-Maqsad <sagar18.iosdeveloper@gmail.com>
2024-05-21 21:45:42 +0500

    Initial commit

---
+++ src/data/user.js
@@ -0,0 +1,90 @@
+const INFO = {
+	main: {
+		title: "iOS | Web | Full Stack",
+		name: "Sagar Ali",
+		email: "sagar18.iosdeveloper@gmail.com",
+		logo: "../sagar-ali.JPG",
+	},
+
+	socials: {
+		twitter: "https://twitter.com/",
+		github: "https://github.com/sagariOSDeveloper",
+		linkedin: "https://linkedin.com/in/sagar-ali-2254b01a6/",
+		instagram: "https://www.instagram.com/s.baloch18",
+	},
+
+	homepage: {
+		title: "Full-stack Web and iOS App Developer",
+		description: `Hello, I'm Sagar Ali, an accomplished Full Stack Web Developer skilled in ReactJS, NextJS, NodeJS, Swift, and SwiftUI. With a focus on crafting high-end iOS mobile apps and web applications. I bring a wealth of experience to every project.
+
+			Throughout my career, I've had the privilege of contributing to numerous high-end iOS mobile app projects and high-end web applications. My expertise lies in creating seamless user experiences that elevate brands and engage audiences.
+			
+			Passionate about leveraging cutting-edge technologies, I'm dedicated to delivering impactful digital solutions that exceed expectations. If you're looking for a skilled developer to bring your project vision to life, let's collaborate and turn ideas into reality.`,
+	},
+
+	about: {
+		title: "I’m Sagar Ali. I live in Karachi Pakistan, where I design the future.",
+		description: `I've had the privilege of contributing to numerous high-end iOS mobile app projects and high-end web applications. My expertise lies in creating seamless user experiences that elevate brands and engage audiences.
+			
+			Passionate about leveraging cutting-edge technologies, I'm dedicated to delivering impactful digital solutions that exceed expectations. If you're looking for a skilled developer to bring your project vision to life, let's collaborate and turn ideas into reality.`,
+	},
+
+	projects: [
+		{
+			title: "Maqsad Web App",
+			description:
+				"Maqsad is an online learning platform for MDCAT, ECAT, BCAT, Class 9-12 students. It is a one-stop solution with video lectures, tests, MCQs, daily classes, study plans and more!",
+			logo: "https://s3-us-west-2.amazonaws.com/cbi-image-service-prd/modified/bdb764ed-03ff-4619-a7e9-acb34731fe49.png",
+			linkText: "View Project",
+			link: "https://maqsad.io/app/mdcat",
+		},
+
+		{
+			title: "Maqsad Website",
+			description:
+				"Maqsad is an online learning platform for MDCAT, ECAT, BCAT, Class 9-12 students. It is a one-stop solution with video lectures, tests, MCQs, daily classes, study plans and more!",
+			logo: "https://s3-us-west-2.amazonaws.com/cbi-image-service-prd/modified/bdb764ed-03ff-4619-a7e9-acb34731fe49.png",
+			linkText: "View Project",
+			link: "https://maqsad.io/",
+		},
+
+		{
+			title: "Maqsad iOS App",
+			description: `Maqsad is an online learning platform for MDCAT, ECAT, BCAT, Class 9-12 students. It is a one-stop solution with video lectures, tests, MCQs, daily classes, study plans and more!`,
+			logo: "https://s3-us-west-2.amazonaws.com/cbi-image-service-prd/modified/bdb764ed-03ff-4619-a7e9-acb34731fe49.png",
+			linkText: "View Project",
+			link: "https://maqsad.io/",
+		},
+
+		{
+			title: "Rove Papers iOS App",
+			description:
+				"Rove Papers is a mobile & web application that gives you access to all O levels, A levels, and, IGCSE past-papers along with multiple features to help you ace your exams.",
+			logo: "https://www.rovepapers.com/rp-small-logo.png",
+			linkText: "View Project",
+			link: "https://apps.apple.com/pk/app/rove-papers-o-a-levels/id1438152142",
+		},
+
+		{
+			title: "Educology iOS App",
+			description:
+				"Educology is a mobile application founded in Dubai, by an expert teacher and her partner in 2020. The application is created to meet the needs of students and parents, by finding the right tutor at the right time in a click.",
+			logo: "https://media.licdn.com/dms/image/C4E03AQGBVAWbcbB0Qw/profile-displayphoto-shrink_400_400/0/1602095339158?e=2147483647&v=beta&t=bikowX9xwxjFi2ppA1zv7KqtmUiL309s_D6liZd40wY",
+			linkText: "View Project",
+			link: "https://apps.apple.com/ae/app/educology/id1545825648",
+		},
+
+		{
+			title: "The Door Step Library iOS App",
+			description: `The Door Step Library is an mobile app that revolutionize the way users discover and access books. With a seamless
+				browsing experience, users can explore hundreds of titles and
+				conveniently order them with a simple click, enjoying doorstep delivery
+				for a hassle-free reading experience`,
+			logo: "https://is1-ssl.mzstatic.com/image/thumb/Purple124/v4/3d/ce/7c/3dce7ce6-eda1-68a1-8b20-f86ce96f6580/AppIcon-0-0-1x_U007emarketing-0-0-0-8-0-0-sRGB-0-0-0-GLES2_U002c0-512MB-85-220-0-0.png/460x0w.webp",
+			linkText: "View Project",
+			link: "https://apps.apple.com/dz/app/the-doorstep-library/id1503896573",
+		},
+	],
+};
+
+export default INFO;
